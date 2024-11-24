fed177cb211ec6175f05a8423f616aab1a8b7075
KartikeyPal <kartikeypal02@gmail.com>
2024-11-24 12:11:20 +0530

    google signin fixed

--- Frontend/src/Component/Navbar/Navbar.jsx
+++ Frontend/src/Component/Navbar/Navbar.jsx
@@ -17,14 +17,13 @@ import {jwtDecode} from "jwt-decode"
 const Navbar = ({ toggledrawer, seteditcreatechanelbtn }) => {
     const [authbtn, setauthbtn] = useState(false)
     const [user, setuser] = useState(null)
-    const [profile, setprofile] = useState([])
+    const [profile, setprofile] = useState(null)
     const dispatch = useDispatch()
     const currentuser = useSelector(state => state.currentuserreducer);
-
+    
     const successlogin = () => {
-        if (profile.email) {
+        if (profile && profile.email) {
             dispatch(login({ email: profile.email }))
-            // console.log(profile.email)
         }
     }
 
@@ -32,37 +31,41 @@ const Navbar = ({ toggledrawer, seteditcreatechanelbtn }) => {
         onSuccess: tokenResponse => {
             setuser(tokenResponse)
         },
-        // `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`
-        //  // Scope => email profile https://www.googleapis.com/auth/userinfo.profile openid https://www.googleapis.com/auth/userinfo.email
         onError: (error) => console.log("Login Failed", error)
     });
+    
+    useEffect(() => {
+        const fetchUserProfile = async() =>{
+        if (user && user.access_token) {
+            try {
+                const res = await axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+                     headers: {
+                         Authorization: `Bearer ${user.access_token}`,
+                         Accept: 'application/json'
+                     }
+                 })
+                setprofile(res.data);
+            } catch (error) {
+                console.error("Error fetching user profile:", error);
+            }
+        }
+    }
+    fetchUserProfile();
+    },[user]);
 
     useEffect(() => {
-       setuser(user);
-        if (user && user.access_token) {
-            axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
-                headers: {
-                    Authorization: `Bearer ${user.access_token}`,
-                    Accept: 'application/json'
-                }
-            })
-            .then((res) => {
-                setprofile(res.data);
-                successlogin();
-            })
-            .catch((error) => {
-                console.error("Error fetching user profile:", error);
-            });
+        if (profile) {
+            successlogin();
         }
-    }, [user]);
-
+    }, [profile]);
+    
     const logout = () => {
         dispatch(setcurrentuser(null));
         googleLogout();
         localStorage.clear();
         console.log("User logged out successfully");
     };
-
+    
     useEffect(()=>{
         const token=currentuser?.token;
         if(token){
@@ -72,10 +75,10 @@ const Navbar = ({ toggledrawer, seteditcreatechanelbtn }) => {
             }
         }
         dispatch(setcurrentuser(JSON.parse(localStorage.getItem("Profile"))))
-  },[currentuser?.token,dispatch]
+    },[currentuser?.token,dispatch]
 )
-    return (
-        <>
+return (
+    <>
             <div className="Container_Navbar">
                 <div className="Burger_Logo_Navbar">
                     <div className="burger" onClick={() => toggledrawer()}>
@@ -110,7 +113,7 @@ const Navbar = ({ toggledrawer, seteditcreatechanelbtn }) => {
                                 <p className="fstChar_logo_App">
                                     {currentuser?.result.name ? (
                                         <>{currentuser?.result.name.charAt(0).toUpperCase()}</>
-
+                                        
                                     ) : (
                                         <>{currentuser?.result.email.charAt(0).toUpperCase()}</>
                                     )}
@@ -138,3 +141,6 @@ const Navbar = ({ toggledrawer, seteditcreatechanelbtn }) => {
 export default Navbar
 
 
+
+// `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`
+//  // Scope => email profile https://www.googleapis.com/auth/userinfo.profile openid https://www.googleapis.com/auth/userinfo.email
